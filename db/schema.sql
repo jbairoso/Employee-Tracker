b8cdf1efd1f215ef05f367523e82bc584562a8c3
jbairoso <jbairoso@gmail.com>
2022-06-10 23:08:42 -0700

    create employee table

--- db/schema.sql
+++ db/schema.sql
@@ -16,9 +16,17 @@ CREATE TABLE department (
     name VARCHAR(30) NOT NULL
 );
 
-CREAT TABLE ROLES (
+CREATE TABLE roles (
     id INTEGER NOT NULL AUTO_INCREMENT PRIMARY_KEY,
     title VARCHAR(30) NOT NULL,
     salary DECIMAL NOT NULL,
-    department_id INTEGER.
-)
+    department_id INTEGER,
+);
+
+CREATE TABLE employee(
+    id INTEGER AUTO_INCREMENT PRIMARY_KEY,
+    first_name VARCHAR(30) NOT NULL,
+    last_name VARCHAR(30) NOT NULL,
+    role_id INTEGER,
+    manager_id INTEGER,
+);
