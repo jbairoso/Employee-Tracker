0e479524f3e9a66be84fae68a86cebaa26ad9844
jbairoso <jbairoso@gmail.com>
2022-06-10 17:23:14 -0700

    lay out requirements in schema

---
+++ db/schema.sql
@@ -0,0 +1,12 @@
+DROP DATABASE IF EXISTS employees;
+CREATE DATABASE employees;
+
+USE employees;
+
+DROP TABLE IF EXISTS department;
+DROP TABLE IF EXISTS roles;
+DROP TABLE IF EXISTS employee;
+
+--table for department
+--table for roles
+--table for employee
