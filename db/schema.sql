51433a49930c2fd9a09d883a61f3db66f9132ce8
jbairoso <jbairoso@gmail.com>
2022-06-10 23:03:13 -0700

    create tables

--- db/schema.sql
+++ db/schema.sql
@@ -10,3 +10,15 @@ DROP TABLE IF EXISTS employee;
 --table for department
 --table for roles
 --table for employee
+
+CREATE TABLE department (
+    id INTEGER AUTO_INCREMENT PRIMARY KEY,
+    name VARCHAR(30) NOT NULL
+);
+
+CREAT TABLE ROLES (
+    id INTEGER NOT NULL AUTO_INCREMENT PRIMARY_KEY,
+    title VARCHAR(30) NOT NULL,
+    salary DECIMAL NOT NULL,
+    department_id INTEGER.
+)
