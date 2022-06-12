5d7e489b5d1f990db49f59d951f393d632d335cd
jbairoso <jbairoso@gmail.com>
2022-06-12 15:41:27 -0700

    fix connection issue

--- db/connection.js
+++ db/connection.js
@@ -4,22 +4,11 @@ const mysql = require("mysql2");
 const connection = mysql.createConnection(
   {
     host: "localhost",
-    port: 3306,
-    // Your MySQL username,
     user: "root",
-    // Your MySQL password
     password: "5228",
     database: "employees",
   },
   console.log("Connected to the employees database.")
 );
-
-connection.connect(function (err) {
-  if (err) {
-    console.error("error connecting: " + err.stack);
-    return;
-  }
-  console.log("connected as id " + connection.threadId);
-});
 
 module.exports = connection;
